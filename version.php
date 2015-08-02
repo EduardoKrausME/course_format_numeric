33fdffb0e48e7bd085a1e9a78384473f34b0713b
Eduardo Kraus <kraus@eduardokraus.com>
2015-08-02 04:22:52 -0300

    Version update

--- version.php
+++ version.php
@@ -28,3 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015080201;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014050800;          // Requires this Moodle version.
 $plugin->component = 'format_numeric';    // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = 'v1.3.1';

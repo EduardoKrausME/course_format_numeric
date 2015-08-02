fd22625ebe69ffec8bc149236d9b917e50345241
Eduardo Kraus <kraus@eduardokraus.com>
2015-08-02 03:16:32 -0300

    Publicação inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# theme_numeric
+Desenvolvi este formato baseado no theme Numeric que esta disponível para download em  [Theme Numeric gratuito e para todos](http://moodle.eduardokraus.com/theme-numeric)
+
+Testei na versão 2.8.
+
+# Como instalar
+Para instala-lo, baixe o fonte deste plug-in em [course_format_numeric.zip](https://github.com/EduardoKrausME/course_format_numeric/raw/master/course_format_numeric.zip) e descompacte-o na pasta moodle/course/format e depois vá ao menu Administração do site >> Avisos e siga os passos para instalação do Plug-in.
---
+++ lang/pt_br/format_numeric.php
@@ -0,0 +1,34 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_numeric', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    format
+ * @subpackage numeric
+ * @copyright  2015 Eduardo Kraus (http://eduardokraus.com)
+ * @license    http://eduardokraus.com
+ */
+
+$string['currentsection'] = 'This topic';
+$string['sectionname']    = 'Numeric';
+$string['pluginname']     = 'Formato Numeric';
+$string['section0name']   = 'General';
+$string['page-course-view-numeric'] = 'Any course main page in numeric format';
+$string['page-course-view-numeric-x'] = 'Any course page in numeric format';
+$string['hidefromothers'] = 'Hide topic';
+$string['showfromothers'] = 'Show topic';
